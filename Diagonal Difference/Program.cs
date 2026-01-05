b77da754bb62336cf66ae438919ae94270985c92
AhmedElgohary10 <gohary200023@gmail.com>
2026-01-05 17:00:07 +0200

    Diagonal Difference problem solved

---
+++ Diagonal Difference/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Diagonal_Difference
+{
+
+    class Result
+    {
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int result;
+            int mainDiagonal = 0;
+            int secDiagonal = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                mainDiagonal += arr[i][i];
+            }
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                secDiagonal += arr[i][arr[i].Count - 1 - i];
+            }
+
+            result = mainDiagonal - secDiagonal;
+            if (result < 0)
+                result = -result;
+
+            return result;
+
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> l1 = new() { 11, 2, 3 };
+            List<int> l2 = new() { 5, 6, 4 };
+            List<int> l3 = new() { 7, 2, 9 };
+
+            List<List<int>> arr = new();
+            arr.Add(l1);
+            arr.Add(l2);
+            arr.Add(l3);
+
+            Console.WriteLine(Result.diagonalDifference(arr));
+
+
+        }
+    }
+}
